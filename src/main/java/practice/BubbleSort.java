f55964a651cfeb8be397dc3f1af03a7667f3a11d
Rahul <rk2670382@gmail.com>
2025-12-18 22:58:02 +0530

    day 6 done

--- src/main/java/practice/BubbleSort.java
+++ src/main/java/practice/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort
     public static void main(String[] args) {
         int[] arr = {4,2,7,1,2}; // Initialize an array
 
-        for (int i=0; i< arr.length-1;i++)
+        for (int i=0; i< arr.length;i++)
         {
             boolean swapped=false;
             for (int j=0; j< arr.length-i-1;j++)
---
+++ src/main/java/practice/InsertionSort.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.Arrays;
+
+public class InsertionSort
+{
+    public static void main(String[] args) {
+        int[] a ={5,4,2,3,9,7,8};
+
+        for (int i=1;i<a.length;i++)
+        {
+            int key=a[i];
+            int j=i-1;
+
+            while (j>=0 && a[j]>key)
+            {
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=key;
+        }
+
+        System.err.println("Insertion Sort \t"+ Arrays.toString(a));
+    }
+}
